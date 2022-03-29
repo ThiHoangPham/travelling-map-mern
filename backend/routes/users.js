bc0a963b540d1e2b12ca25f89b8a2a0d08786536
Adam Pham <thaihoangpham2008@gmail.com>
2022-03-29 22:54:42 +0800

    finished resgister/users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Pin = require("../models/User");
 const bcrypt = require('bcrypt');
+const User = require("../models/User");
 
 // resgister
 
@@ -8,18 +9,29 @@ router.post("/register", async (req, res) => {
     try {
         // generate new password
         const salt = await bcrypt.genSalt(10);
-        const 
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
         // create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        })
 
         // save user and send response
-
+        const user = await newUser.save();
+        res.status(200).json(user._id)
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
 
 // login
+router.post("/login", async (req, res) => {
+    try{
+        // find user
 
-
+    }catch
+})
 
 module.exports = router;
