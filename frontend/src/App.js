0b30a1b8dda33df116f04f61b4a1d98486ee0d76
Adam Pham <thaihoangpham2008@gmail.com>
2022-04-05 08:51:18 +0800

    customize popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import Map, { Marker } from 'react-map-gl';
-
+import Map, { Marker, Popup } from 'react-map-gl';
+import { Room, Star } from "@material-ui/icons";
+import "./app.css";
 
 function App() {
   return (
@@ -14,13 +15,39 @@ function App() {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
+
       <Marker
         latitude={48.858093}
         longitude={2.294694}
         offsetLeft={-20}
         offsetTop={-10}>
-      </Marker>
-    </Map>
+        <Room style={{ fontSize: visualViewport.zoom * 7, color: "slateblue" }} /></Marker>
+
+      <Popup
+        latitude={48.858093}
+        longitude={2.294694}
+        closeButton={true}
+        closeOnClick={false}
+        anchor="left">
+        <div className='card'>
+          <label>Place</label>
+          <h4 className='place'>Eiffell Tower</h4>
+          <label>Review</label>
+          <p>Beautifull place. I like it.</p>
+          <label>Rating</label>
+          <div className='stars'>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <label>Information</label>
+          <span className='username'>Created by<b>Adam</b></span>
+          <span className='date'> 2 hours ago</span>
+        </div>
+      </Popup>
+    </Map >
   )
 }
 export default App;
