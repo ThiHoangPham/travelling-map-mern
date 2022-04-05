911ef62d41d2086ec6b113c312497252938b1154
Adam Pham <thaihoangpham2008@gmail.com>
2022-04-05 11:14:32 +0800

    backend - frontend connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Map, { Marker, Popup } from 'react-map-gl';
 import { Room, Star } from "@material-ui/icons";
 import "./app.css";
+import axios from "axios";
 
 function App() {
+  const [pins, setPins] = useState([]);
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
   return (
     <Map
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
@@ -15,38 +29,41 @@ function App() {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
+      {pins.map(p => (
+        <>
+          <Marker
+            latitude={p.lat}
+            longitude={p.long}
+            offsetLeft={-20}
+            offsetTop={-10}>
+            <Room style={{ fontSize: visualViewport.zoom * 7, color: "slateblue" }} /></Marker>
 
-      <Marker
-        latitude={48.858093}
-        longitude={2.294694}
-        offsetLeft={-20}
-        offsetTop={-10}>
-        <Room style={{ fontSize: visualViewport.zoom * 7, color: "slateblue" }} /></Marker>
-
-      {/* <Popup
-        latitude={48.858093}
-        longitude={2.294694}
-        closeButton={true}
-        closeOnClick={false}
-        anchor="left">
-        <div className='card'>
-          <label>Place</label>
-          <h4 className='place'>Eiffell Tower</h4>
-          <label>Review</label>
-          <p className='desc'>Beautifull place. I like it.</p>
-          <label>Rating</label>
-          <div className='stars'>
-            <Star className='star' />
-            <Star className='star' />
-            <Star className='star' />
-            <Star className='star' />
-            <Star className='star' />
-          </div>
-          <label>Information</label>
-          <span className='username'>Created by <b>Adam</b></span>
-          <span className='date'> 2 hours ago</span>
-        </div>
-      </Popup> */}
+          <Popup
+            latitude={p.lat}
+            longitude={p.long}
+            closeButton={true}
+            closeOnClick={false}
+            anchor="left">
+            <div className='card'>
+              <label>Place</label>
+              <h4 className='place'>{p.title}</h4>
+              <label>Review</label>
+              <p className='desc'>{p.desc}</p>
+              <label>Rating</label>
+              <div className='stars'>
+                <Star className='star' />
+                <Star className='star' />
+                <Star className='star' />
+                <Star className='star' />
+                <Star className='star' />
+              </div>
+              <label>Information</label>
+              <span className='username'>Created by <b>{p.username}</b></span>
+              <span className='date'> 2 hours ago</span>
+            </div>
+          </Popup>
+        </>
+      ))}
     </Map >
   )
 }
